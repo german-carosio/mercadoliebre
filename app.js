221d005e9f178f666ffb3d23eafd6e490e3cbb04
German Carosio <carosiogerman@gmail.com>
2022-04-25 21:24:23 -0300

    mejoras

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ app.listen(process.env.PORT || 3030, () => {
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/views/index.html'))
+  res.sendFile(path.resolve(__dirname, 'views/index.html'))
 })
 
 app.get('/register', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/views/register.html'))
+  res.sendFile(path.resolve(__dirname, 'views/register.html'))
 })
 
 app.get('/login', (req,res) => {
-  res.sendFile(path.resolve(__dirname, '/views/login.html'))
+  res.sendFile(path.resolve(__dirname, 'views/login.html'))
 })
 
 app.post('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/views/index.html'))
+  res.sendFile(path.resolve(__dirname, 'views/index.html'))
 })
 
